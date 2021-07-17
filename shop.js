2c6b8199f2024c6ba92348d52f48d59a3b863e18
wohuowo <tonyobriku@gmail.com>
2021-07-17 14:33:05 +0100

    js tutorials

---
+++ shop.js
@@ -0,0 +1,16 @@
+var myList = [["cereal", 3], ["apple", 5], ["orange", 7], ["juice", 8]];
+console.log(myList);
+// reusable code with functions 
+function ourReuseableFunction() {
+    console.log("Tony", "Obriku");
+
+}
+
+ourReuseableFunction()
+ourReuseableFunction()
+ourReuseableFunction()
+// Passing values to Functions with Argument
+function ourName (x, y) {
+    console.log(x-y);
+}
+ourName(100, 30);
